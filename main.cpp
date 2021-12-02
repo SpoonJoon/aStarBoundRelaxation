2f1fd945f7ae4cd5ade70d0001451002e262ad61
SpoonJoon <54812775+SpoonJoon@users.noreply.github.com>
2021-12-02 13:50:16 -0500

    findNode added

--- main.cpp
+++ main.cpp
@@ -11,12 +11,12 @@ struct Node {
 	std::vector<std::pair<Node*, double> > successors;
 };
 
-template<class T, class C = std::vector<T>, class P = std::less<typename C::value_type> >
-struct MyPriorityQueue : std::priority_queue<T,C,P> {
+//MinPriorityQueue
+template<class T, class C = std::vector<T>, class P = std::greater<typename C::value_type> >
+struct MinQueue : std::priority_queue<T,C,P> {
    typename C::iterator begin() { return std::priority_queue<T, C, P>::c.begin(); }
    typename C::iterator end() { return std::priority_queue<T, C, P>::c.end(); }
 };
-
 
 void reconstructPath(Node* predecessors[], Node* current) {
 	// From wikipedia. Correctness not guaranteed
@@ -27,10 +27,19 @@ void reconstructPath(Node* predecessors[], Node* current) {
 	// return the path
 }
 
+bool findNode(MinQueue<Node*> &openSet, Node* node) {
+	for (auto itr = openSet.begin(); itr!=openSet.end(); itr++){
+		if(*itr == node)
+			return true;
+	}
+	return false;
+}
 
 void aStar(Node& startPt, Node& goalPt, Node nodes[], std::size_t n, std::function<double(Node&)> heuristic) {
+	
 	//std::priority_queue<Node*> openSet;
-	MyPriorityQueue<Node*> openSet;
+	//new queue
+	MinQueue<Node*> openSet;
 	openSet.push(&startPt);
 
 	// Stores the preceding node for each node on the most optimal path thus far
@@ -61,28 +70,14 @@ void aStar(Node& startPt, Node& goalPt, Node nodes[], std::size_t n, std::functi
 				estimatedDistance[successor.first->i] = distance + heuristic(*successor.first);
 				// If the successor is not in openSet, then
 				//	openSet.push(successor.first);
+				if(findNode(openSet, successor.first))
+					openSet.push(successor.first);
 			}
-
 		}
-
-
 	}
 
 }
 
 int main(int argc, char** argv) {
 
-	MyPriorityQueue<Node*> openSet;	
-	Node node1, node2, node3;
-	node1.i = 2;
-	node2.i = 1;
-	node3.i = 4;
-	openSet.push(&node1);
-	openSet.push(&node2);
-	openSet.push(&node3);
-
-	for (auto itr = openSet.begin(); itr!=openSet.end(); itr++)
-	{
-		std::cout << (*itr)->i << std::endl;
-	}
 }
