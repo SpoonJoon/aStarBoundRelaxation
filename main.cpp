e19ad6f675740b07241de8843d87ceb6f6f9a45c
stephen <sblack3@binghamton.edu>
2021-12-03 19:11:49 -0500

    added parsing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,47 @@
 #include "astar.hpp"
 #include <iostream>
+#include <fstream>
 
-int main() {
+int main(int argc, char** argv) {
+
+	std::string input = argv[1];
+	
+    std::string temp;
+    std::ifstream lim(input);
+	getline(lim,temp);
+	int dXsize = 0;
+	int dYsize = 0;
+	std::string x = "";
+	std::string y = "";
+	int flag = 0;
+	for (auto i:temp){
+		if(i == ' '){
+			flag =1;
+		} else if(i == 0){
+			x+=i;
+		} else if(i ==1){
+			y+=i;
+		}
+
+	}
+	dXsize = stoi(x);
+	dYsize = stoi(y);
+	bool nodes[dXsize][dYsize];
+	for(int i = 0; i < dXsize;i++){
+		for(int j = 0; j < dXsize;j++){
+			getline(lim,temp);
+			if(stoi(temp) == 0){
+			nodes[i][j] = false;
+			} else {
+				nodes[i][j] = true;
+			}
+		
+		}
+		
+	}
+
+        
+
     const int xSize = 8;
     const int ySize = 4;
     bool blocked[ySize][xSize] = {
